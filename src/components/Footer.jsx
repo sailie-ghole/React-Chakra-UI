7a9a54dca2e9984273f8d55ed65ebcf4316cd4cc
sailie-ghole <gholesailie1410@gmail.com>
2023-09-12 02:51:22 +0530

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Box, Container, Heading, Image, Stack, Text, Button,
+    VStack,
+    HStack, Input } from '@chakra-ui/react';
+import { AiOutlineSend } from 'react-icons/ai';
+
+
+const Footer = () => {
+  return (
+    <Box bgColor={'blackAlpha.900'} minH={'40'} p={'16'} color={'white'}>
+        <Stack direction={['column', 'row']}>
+            
+            <VStack alignItems={'stretch'} w={'full'} px={'4'}>
+                <Heading size="md" textTransform={'uppercase'}>
+                    Subscribe to get Updates
+                </Heading>
+                <HStack>
+                    <Input
+                        placeholder="Enter Email Here..."
+                        border={'none'}
+                        borderRadius="none"
+                        outline={'none'}
+                        focusBorderColor="none"
+                    />
+                    <Button
+                        p={'0'}
+                        colorScheme={'purple'}
+                        variant={'ghost'}
+                        borderRadius={'0 20px 20px 0'}
+                        >
+                        <AiOutlineSend size={20} />
+                    </Button>
+                </HStack>
+            </VStack>
+
+            <VStack 
+                w={'full'}
+                borderLeft={['none', '1px solid white']}
+                borderRight={['none', '1px solid white']}
+            >
+                <Heading textTransform={'uppercase'} textAlign={'center'}>
+                    VIDEO HUB
+                </Heading>
+                <Text>All rights received</Text>
+            </VStack>
+
+            <VStack w={'full'}>
+                <Heading size={'md'} textTransform={'uppercase'}>
+                    Social Media
+                </Heading>
+                <Button variant={'link'} colorScheme={'white'}>
+                    <a target={'black'} href="https://youtube.com/6packprogrammer">
+                    Youtube
+                    </a>
+                </Button>
+                <Button variant={'link'} colorScheme={'white'}>
+                    <a target={'black'} href="https://instagram.com/meabhisingh">
+                    Instagram
+                    </a>
+                </Button>
+                <Button variant={'link'} colorScheme={'white'}>
+                    <a target={'black'} href="https://github.com/meabhisingh">
+                    Github
+                    </a>
+                </Button> 
+            </VStack>
+
+        </Stack>
+    </Box>
+  )
+}
+
+export default Footer
